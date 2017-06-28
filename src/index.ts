443f3fd43f743646815d62990732e0daef0d4b4f
kimu_shu <alfvegardrisc@gmail.com>
2017-06-29 00:17:30 +0900

    Clean up serial emulation

--- src/index.ts
+++ src/index.ts
@@ -12,39 +12,46 @@ export function getModuleConstructor(kind: string) {
     return PeridotHostBridgeModule;
 }
 
-function loopPromise(start: number, end: number, step: number, action: (offset: number) => any, result?: any): Promise<any> {
-    let next = (now: number, result: any) => {
-        if (((step > 0) && (now < end)) || ((step < 0) && (now > end))) {
-            return Promise.resolve(action(now)).then((result) => {
-                return next(now + step, result);
-            })
-        }
-        return Promise.resolve(result);
-    }
-    return next(start, result);
-}
-
 class PeridotHostBridgeModule extends Module {
+    /* Interfaces */
     public m1: AvalonMaster;
     public s1: AvalonSlave;
     public avsirq: InterruptSender;
+
+    /** LED mask */
     protected ledMask: number = 0xf;
 
+    /** Port name (COMxx, /dev/xxx) */
     private _port: string;
+    /** Serial port instance */
     private _serial: SerialPort;
+
+    /** Class ID (from sopcinfo) */
     private _classid: number;
+    /** Timecode (from sopcinfo) */
     private _timecode: number;
+    /** Unique ID Low word (from simulator option) */
     private _uid_l: number;
+    /** Unique ID High word (from simulator option) */
     private _uid_h: number;
+    /** Board ID (from simulator option) */
     private _bid: string;
+    /** Serial ID (from simulator option) */
     private _sid: string;
 
-    private _key: number;
+    /** Reset key */
+    private _resetKey: number;
+    /** UID valid */
     private _uidval: boolean;
+    /** UID feature enabled */
     private _uid_en: boolean;
+    /** Flash feature enabled */
     private _flash_en: boolean;
+    /** Message/SWI feature enabled */
     private _msg_en: boolean;
+    /** User configuration image enabled */
     private _user_img: boolean;
+    /** LED status */
     private _led: number;
 
     private _irqena: boolean;
@@ -62,7 +69,6 @@ class PeridotHostBridgeModule extends Module {
     private _lastCommand: number;
     private _i2cByte: number;
     private _i2cDrive: number;
-    private _i2cDriveNext: number;
     private _i2cState: number;
     private _i2cWrite: boolean;
     private _eepAddr: number;
@@ -104,7 +110,7 @@ class PeridotHostBridgeModule extends Module {
     load(moddesc: SopcInfoModule): Promise<void> {
         let a = moddesc.assignment;
         let i = moddesc.interface;
-        this._key = parseInt(a.embeddedsw.CMacro.CPURESET_KEY);
+        this._resetKey = parseInt(a.embeddedsw.CMacro.CPURESET_KEY);
         this._classid = parseInt(a.embeddedsw.CMacro.ID);
         this._timecode = parseInt(a.embeddedsw.CMacro.TIMESTAMP);
         this._flash_en = (a.embeddedsw.CMacro.USE_EPCSBOOT === "1");
@@ -121,9 +127,10 @@ class PeridotHostBridgeModule extends Module {
 
         this._lastCommand = 0x39;
         this._i2cDrive = 0x30;
-        this._i2cDriveNext = 0x30;
         this._i2cState = -2;
         this._eepAddr = 0;
+
+        // Initialize EEPROM
         this._eepData = new Uint8Array(256);
         this._eepData.set([0x4a, 0x37, 0x57, 0x02], 0);
         this._eepData.set(Buffer.from(this._bid), 4);
@@ -147,13 +154,14 @@ class PeridotHostBridgeModule extends Module {
                     console.log(`[peridot_hostbridge] Using port: ${this._port}`);
                     resolve();
                 });
-                this._serial.on("data", (data) => {
+                this._serial.flush();
+                this._serial.on("data", (data: Buffer) => {
                     this._receivePromise = this._receivePromise.then(() => {
-                        return this._receiveSerial(data)
+                        return this._recvSerial(data)
                     });
                 });
                 this._serial.on("error", (error) => {
-                    console.error("[peridot_hostbridge] serial error:", error);
+                    console.error(`[peridot_hostbridge] serial error: ${error}`);
                 });
             });
         }
@@ -163,26 +171,33 @@ class PeridotHostBridgeModule extends Module {
         });
     }
 
-    private _receiveSerial(data: Buffer): Promise<void> {
+    /**
+     * Process received data from serial
+     * @param data Received data
+     */
+    private _recvSerial(data: Buffer): Promise<void> {
         return data.reduce((promise, byte) => {
             return promise
             .then(() => {
                 if (byte === 0x3a) {
                     // Command byte for PERIDOT
-                    this._receiver = this._receiveCommand;
+                    this._receiver = this._recvCommand;
                     return;
                 }
                 if (!this._receiver) {
-                    this._receiver = this._receiveStart;
+                    this._receiver = this._recvPacket;
                 }
                 return this._receiver(byte);
             });
         }, Promise.resolve());
     }
 
-    private _receiveStart(byte: number): Promise<void> | void {
+    /**
+     * Process data for Avalon-ST packet
+     * @param byte Byte received
+     */
+    private _recvPacket(byte: number): Promise<void> | void {
         let packet: number[];
-        // console.log(`receiveStart: 0x${byte.toString(16)}`);
         if (byte === 0x3d) {
             this._escaped3 = true;
             return;
@@ -194,7 +209,7 @@ class PeridotHostBridgeModule extends Module {
         switch (byte) {
             case 0x7c:
                 // Channel prefix
-                this._receiver = this._receiveChannel;
+                this._receiver = this._recvChannel;
                 return;
             case 0x7a:
                 // SOP
@@ -221,15 +236,20 @@ class PeridotHostBridgeModule extends Module {
             packet.push(byte);
             if (packet["eop"]) {
                 this._packets[this._channel] = null;
-                this._processPacket(Buffer.from(packet));
+                return this._processPacket(Buffer.from(packet));
             }
         }
     }
 
-    private _receiveCommand(byte: number): void {
+    /**
+     * Process configuration command byte
+     * @param byte Byte received
+     */
+    private _recvCommand(byte: number): void {
         if ((byte & 0x01) === 0) {
             throw new Error("nCONFIG asserted by hostbridge");
         }
+        let i2cDriveOld = this._i2cDrive;
         if ((byte & 0x80) === 0) {
             // I2C emulation
             let change = (byte ^ this._lastCommand) & 0x30;
@@ -242,12 +262,12 @@ class PeridotHostBridgeModule extends Module {
                     // fall edge (change)
                     ++this._i2cState;
                     if (this._i2cState < 8) {
-                        this._i2cDriveNext = 0x30;
+                        this._i2cDrive = 0x30;
                     } else if (this._i2cState === 8) {
                         if ((this._i2cByte >>> 1) === EEPROM_SLAVE_ADDR) {
-                            this._i2cDriveNext = 0x10;  // ACK
+                            this._i2cDrive = 0x10;  // ACK
                         } else {
-                            this._i2cDriveNext = 0x30;  // NACK
+                            this._i2cDrive = 0x30;  // NACK
                             this._i2cState = -2;    // Ignore transaction
                         }
                         this._i2cWrite = (this._i2cByte & 0x01) === 0;
@@ -259,18 +279,18 @@ class PeridotHostBridgeModule extends Module {
                             } else {
                                 // Write protected
                             }
-                            this._i2cDriveNext = 0x10;  // ACK
+                            this._i2cDrive = 0x10;  // ACK
                         } else {
-                            this._i2cDriveNext = 0x30;  // Release
+                            this._i2cDrive = 0x30;  // Release
                         }
                     } else {
                         // Read (Slave -> Master)
                         let bit = (this._i2cState % 9);
                         if (bit === 8) {
                             ++this._eepAddr;
-                            this._i2cDriveNext = 0x30;  // Release
+                            this._i2cDrive = 0x30;  // Release
                         } else {
-                            this._i2cDriveNext = 0x10 | (((this._eepData[this._eepAddr] >> (7 - bit)) & 1) << 5);
+                            this._i2cDrive = 0x10 | (((this._eepData[this._eepAddr] >> (7 - bit)) & 1) << 5);
                         }
                     }
                 }
@@ -284,151 +304,110 @@ class PeridotHostBridgeModule extends Module {
                     this._i2cState = -1;
                     this._i2cByte = 0;
                 }
-                this._i2cDriveNext = 0x30;
+                this._i2cDrive = 0x30;
             }
         }
         this._serial.write(Buffer.alloc(1,
             // AS mode, nSTATUS=H, CONF_DONE=H
-            0x07 | (this._lastCommand & this._i2cDrive)
+            0x07 | (this._lastCommand & i2cDriveOld)
         ));
         this._lastCommand = byte;
-        this._i2cDrive = this._i2cDriveNext;
         this._receiver = null;
     }
 
-    private _receiveChannel(byte: number): void {
+    /**
+     * Receive channel indicator for Avalon-ST packet
+     * @param byte Byte received
+     */
+    private _recvChannel(byte: number): void {
         this._channel = byte;
         this._receiver = null;
     }
 
+    /**
+     * Process Avalon-ST packet
+     * @param packet Packet data
+     */
     private _processPacket(packet: Buffer): Promise<void> {
+        let ofst: number;
         let addr: number;
         let size: number;
-        let incl: number = 1;
-        let promise: Promise<any>;
-        let pre: number;
-        let mid: number;
-        let post: number;
-        let buf: Buffer;
+        let incl: boolean = true;
+        let resp: Buffer;
 
-        console.log(`processPacket(0x${packet.readUInt8(0).toString(16)})`);
-        switch (packet.readUInt8(0)) {
+        let type = packet.readUInt8(0);
+        switch (type) {
             case 0x00:  // Write, non-incrementing address
-                incl = 0;
+                incl = false;
                 /* fall through */
             case 0x04:  // Write, incrementing address
                 addr = packet.readUInt32BE(4);
                 size = packet.readUInt16BE(2);
-                pre = (4 - (addr & 3)) & 3;
-                mid = (size - pre) & ~3;
-                post = size - (pre + mid);
-                return Promise.resolve()
-                .then(() => {
-                    return loopPromise(
-                        0, pre, 1,
-                        (offset) => this.m1.write8(addr + incl * offset, packet.readUInt8(8 + offset))
-                    );
-                })
-                .then(() => {
-                    return loopPromise(
-                        pre, pre + mid, 4,
-                        (offset) => this.m1.write32(addr + incl * offset, packet.readUInt32LE(8 + offset))
-                    );
-                })
-                .then(() => {
-                    return loopPromise(
-                        pre + mid, pre + mid + post, 1,
-                        (offset) => this.m1.write8(addr + incl * offset, packet.readUInt8(8 + offset))
-                    );
-                })
-                .then(() => {
-                    let buf: Buffer = Buffer.allocUnsafe(4);
-                    buf[0] = packet[0] ^ 0x80;
-                    buf[1] = 0;
-                    buf.writeUInt16BE(size, 2);
-                    return this._sendPacket(0, buf);
-                });
+                ofst = 8;
+                resp = Buffer.from([type ^ 0x80, 0, 0, 0]);
+                resp.writeUInt16BE(size, 2);
+                let write = () => {
+                    let p;
+                    if ((size >= 4) && ((addr & 3) === 0)) {
+                        p = this.m1.write32(addr, packet.readUInt32LE(ofst));
+                        if (incl) { addr += 4; }
+                        size -= 4;
+                        ofst += 4;
+                    } else {
+                        p = this.m1.write8(addr, packet.readUInt8(ofst));
+                        if (incl) { addr += 1; }
+                        size -= 1;
+                        ofst += 1;
+                    }
+                    return Promise.resolve(p)
+                    .then(() => (size > 0) ? write() : null);
+                };
+                return write().then(() => this._sendPacket(0, resp));
             case 0x10:  // Read, non-incrementing address
-                addr = packet.readUInt32BE(4);
-                size = packet.readUInt16BE(2);
-                return loopPromise(
-                    0, size, 4,
-                    () => this.m1.read32(addr, 4),
-                    new Uint32Array([0])
-                )
-                .then((i32: Uint32Array) => {
-                    console.log(`read addr=0x${addr.toString(16)}, size=0x${size.toString(16)}, result=${i32}`);
-                    return this._sendPacket(0, Buffer.from(i32.buffer, i32.byteOffset, size));
-                });
+                incl = false;
+                /* fall through */
             case 0x14:  // Read, incrementing address
                 addr = packet.readUInt32BE(4);
                 size = packet.readUInt16BE(2);
-                pre = (4 - (addr & 3)) & 3;
-                mid = (size - pre) & ~3;
-                post = size - (pre + mid);
-                buf = Buffer.allocUnsafe(size);
-                return Promise.resolve()
-                .then(() => {
-                    let readPre = (offset: number): Promise<void> => {
-                        let len = pre - offset;
-                        return Promise.resolve(this.m1.read8(addr + offset, len))
-                        .then((i8) => {
-                            if (i8) {
-                                buf.set(i8.subarray(0, len), offset);
-                                offset += i8.length;
-                                if (offset < pre) {
-                                    return readPre(offset);
-                                }
+                resp = Buffer.allocUnsafe(size);
+                let read = () => {
+                    let p;
+                    if ((size >= 4) && ((addr & 3) === 0)) {
+                        p = this.m1.read32(addr, size & ~3);
+                    } else {
+                        p = this.m1.read8(addr, size);
+                    }
+                    return Promise.resolve(p)
+                    .then((i: Int32Array | Int8Array) => {
+                        if (!i) {
+                            if ((size >= 4) && ((addr & 3) === 0)) {
+                                i = new Int32Array([0]);
+                            } else {
+                                i = new Int8Array([0]);
                             }
-                        })
-                    };
-                    return readPre(0);
-                })
-                .then(() => {
-                    let readMid = (offset: number): Promise<void> => {
-                        let len = (pre + mid) - offset;
-                        return Promise.resolve(this.m1.read32(addr + offset, len))
-                        .then((i32) => {
-                            if (i32) {
-                                buf.set(
-                                    new Uint8Array(i32.buffer, i32.byteOffset + offset, i32.byteLength)
-                                    .subarray(0, len),
-                                    offset
-                                );
-                                offset += i32.byteLength;
-                                if (offset < (pre + mid)) {
-                                    return readMid(offset);
-                                }
-                            }
-                        })
-                    };
-                    return readMid(pre);
-                })
-                .then(() => {
-                    let readPost = (offset: number): Promise<void> => {
-                        let len = size - offset;
-                        return Promise.resolve(this.m1.read8(addr + offset, len))
-                        .then((i8) => {
-                            if (i8) {
-                                buf.set(i8.subarray(0, len), offset);
-                                offset += i8.length;
-                                if (offset < size) {
-                                    return readPost(offset);
-                                }
-                            }
-                        });
-                    };
-                    return readPost(pre + mid);
-                })
-                .then(() => {
-                    return this._sendPacket(0, buf);
-                });
+                        }
+                        let len = Math.min(i.byteLength, size);
+                        resp.set(Buffer.from(i.buffer, i.byteOffset, len), ofst);
+                        if (incl) { addr += len; }
+                        size -= len;
+                        ofst += len;
+                        return (size > 0) ? read() : null;
+                    });
+                };
+                return read().then(() => this._sendPacket(0, resp));
             case 0x7f:  // No transaction
             default:    // Unsupported transaction codes
-                return Promise.resolve(this._sendPacket(0, Buffer.from([0xff, 0x00, 0x00, 0x00])));
+                return Promise.resolve(
+                    this._sendPacket(0, Buffer.from([0xff, 0x00, 0x00, 0x00]))
+                );
         }
     }
 
+    /**
+     * Send packet
+     * @param channel Avalon-ST channel
+     * @param data Packet content
+     */
     private _sendPacket(channel: number, data: Buffer): void {
         let encoded: Buffer = Buffer.allocUnsafe(data.length * 2 + 4);
         let n = 0;
@@ -452,14 +431,14 @@ class PeridotHostBridgeModule extends Module {
                     encoded[n++] = byte;
             }
         }
-        encoded = encoded.slice(0, n);
-        console.log(`packet ${encoded.length} bytes:`);
-        console.log(Array.from(encoded).map((v) => `0${v.toString(16)}`.substr(-2)).join(", "));
         this._serial.write(encoded.slice(0, n));
     }
 
+    /**
+     * CSR read emulation
+     * @param offset Register offset
+     */
     private _readReg(offset: number): number {
-        console.log(`SWI readreg: ${offset}`);
         switch (offset) {
             case 0:
                 return this._classid;
@@ -494,6 +473,11 @@ class PeridotHostBridgeModule extends Module {
         }
     }
 
+    /**
+     * CSR write emulation
+     * @param offset Register offset
+     * @param value Value to write
+     */
     private _writeReg(offset: number, value: number): boolean {
         switch (offset) {
             case 0:
@@ -506,7 +490,7 @@ class PeridotHostBridgeModule extends Module {
                 this._led = value & this.ledMask;
                 if (value & 0x0100) {
                     // Reset
-                    if ((this._key !== 0) || (this._key === (value >>> 16))) {
+                    if ((this._resetKey !== 0) || (this._resetKey === (value >>> 16))) {
                         throw new Error("Nios II reset by PERIDOT SWI");
                     } else {
                         this.options.printWarn("Nios II reset requested with incorrect key");
